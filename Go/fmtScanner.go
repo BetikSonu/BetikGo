a7b278d8e9a5913162ce87a22e2be5460a5d4ed7
raifpy <omerto12.45@gmail.com>
2020-07-15 18:10:04 +0300

    @BetikSonu | 2

--- Go/fmtScanner.go
+++
@@ -1,7 +0,0 @@
-package main
-import "fmt"
-func main(){
-	abc:=fmt.Scanner("asd",1)
-	fmt.Println(abc)
-
-}
--- Go/merhaba.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Merhaba Dünya")
-	/*for (i int;i<10;i++){
-	fmt.Println(i)*/
-}
